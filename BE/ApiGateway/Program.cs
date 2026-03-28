8a89dc9b0499d3ce8bc0151bff3ab1e475a4243e
buikimtrai <buikimtrai2k5@gmail.com>
2026-03-28 16:53:28 +0700

    Cau hinh file ocelot

--- BE/ApiGateway/Program.cs
+++ BE/ApiGateway/Program.cs
@@ -1,36 +1,17 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
-namespace ApiGateway
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+// Yêu cầu hệ thông nạp cấu hình từ file ocelot.json 
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true); 
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+// Đăng ký dịch vụ ocelot
+builder.Services.AddOcelot(builder.Configuration);
+var app = builder.Build();
 
-            var app = builder.Build();
+app.MapGet("/", () => "Hello from API Gateway!");
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
-}
+// 3. Sử dụng Middleware của Ocelot (Phải dùng await) 
+await app.UseOcelot();
+app.Run(); 
