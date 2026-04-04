ba22011f810b909901681e7d77f2b488a2c7ce63
buikimtrai <buikimtrai2k5@gmail.com>
2026-04-04 14:55:32 +0700

    them navbar

---
+++ FE/VocabJourney/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BookOpen } from "lucide-react";
+import "./Navbar.css"
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="container nav-container">
+          {/* logo bên trái */}
+          <div className="logo-section">
+            <div className="logo">
+              <BookOpen size={24} color="while"></BookOpen>
+            </div>
+            <span className="logo-text">VocabJourney</span>
+          </div>
+          {/* cta bên phải */}
+          <div className="nav-actions">
+            <button className="btn-login">Login</button>
+            <button className="btn-get-started">Get Started</button>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
